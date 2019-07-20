86c8d254612a58e8df7411825d93bb3301b5db49
Hugo Hervieux <hugo@shaka.xyz>
2019-07-20 21:42:33 +0200

    Add manifest retrieving and parsing

--- retag.go
+++ retag.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -9,16 +10,24 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type imageRef struct {
+// ImageRef : The image reference, but parsed. Contains registry, repository, tag.
+type ImageRef struct {
 	registry   string
 	repository string
 	tag        string
 }
 
+// Image : An image manifest, the layers and the config are parsed.
+type Image struct {
+	layers   []string
+	config   string
+	manifest interface{}
+}
+
 func main() {
 	log.SetLevel(log.DebugLevel)
-	source := "registry.localhost/image1:coucou"
-	dest := "registry.localhost/image2"
+	source := "reg.localhost/image1:v1"
+	dest := "reg.localhost/image2"
 	sourceRef, err := parseImageRef(source)
 	if err != nil {
 		fatal("Invalid source image ref %s", err)
@@ -31,9 +40,10 @@ func main() {
 		fatal("Images not in the same registry", nil)
 	}
 
-	url := sourceRef.registry
 	scheme := "http://"
-	logged, err := checkRegistry(fmt.Sprintf("%s%s/v2/", scheme, url))
+	url := fmt.Sprintf("%s%s/v2/", scheme, sourceRef.registry)
+	log.Debugf("Base url : %s", url)
+	logged, err := checkRegistry(url)
 	if err != nil {
 		fatal("Error checking the registry : %s", err)
 	}
@@ -41,7 +51,8 @@ func main() {
 		fatal("Login not implemented yet", nil)
 	}
 	log.Infof("Registry reachable")
-
+	image := getManifest(url, sourceRef)
+	log.Debugf("Image data : %s", image)
 }
 
 func fatal(format string, err error) {
@@ -53,11 +64,11 @@ func fatal(format string, err error) {
 	os.Exit(1)
 }
 
-func parseImageRef(ref string) (imageRef, error) {
+func parseImageRef(ref string) (ImageRef, error) {
 	parts := strings.Split(ref, "/")
 	if len(parts) < 2 {
 		err := fmt.Errorf("Error parsing image ref, no registry found : %s", ref)
-		return imageRef{}, err
+		return ImageRef{}, err
 	}
 	registry := parts[0]
 	var tag string
@@ -73,17 +84,64 @@ func parseImageRef(ref string) (imageRef, error) {
 		log.Debugf("tag : %s", tag)
 	default:
 		err := fmt.Errorf("Error parsing image ref, multiple ':' in last part : %s", ref)
-		return imageRef{}, err
+		return ImageRef{}, err
 	}
 
 	repository := strings.Join(append(parts[1:len(parts)-1], lastPart[:1]...), "/")
 
-	return imageRef{registry, repository, tag}, nil
+	return ImageRef{registry, repository, tag}, nil
 }
 
 func checkRegistry(url string) (bool, error) {
-	resp, err := http.Get(url)
-	log.Debugf("Registry response : %v", resp)
+	res, err := http.Get(url)
+	log.Debugf("Registry response : %v", res)
+	defer res.Body.Close()
 
-	return resp.StatusCode == 200, err
+	return res.StatusCode == http.StatusOK, err
+}
+
+// Manifest : Struct used to unmarshall a v2 manifest
+type Manifest struct {
+	SchemaVersion int        `json:"schemaVersion"`
+	MediaType     string     `json:"mediaType"`
+	Config        BlobInfo   `json:"config"`
+	Layers        []BlobInfo `json:"layers"`
+}
+
+// BlobInfo : Struct used to unmarshall a v2 manifest blob info
+type BlobInfo struct {
+	MediaType string `json:"mediaType"`
+	Size      int    `json:"size"`
+	Digest    string `json:"digest"`
+}
+
+func getManifest(url string, img ImageRef) Image {
+	url = fmt.Sprintf("%s%s/manifests/%s", url, img.repository, img.tag)
+	log.Debugf("Getting manifest at URL : %s", url)
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Accept", "application/vnd.docker.distribution.manifest.v2+json")
+	res, err := client.Do(req)
+	if err != nil {
+		fatal("Error requesting the manifest : %s", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		fatal("Non 200 status code recieved when requesting manifest", nil)
+	}
+
+	manifest := Manifest{}
+	err = json.NewDecoder(res.Body).Decode(&manifest)
+	if err != nil {
+		fatal("Error reading JSON : %s", err)
+	}
+	log.Debugf("Decoded manifest : %v", manifest)
+
+	layers := make([]string, 0)
+	config := manifest.Config.Digest
+	for i := 0; i < len(manifest.Layers); i++ {
+		layers = append(layers, manifest.Layers[i].Digest)
+	}
+	return Image{layers, config, manifest}
 }
